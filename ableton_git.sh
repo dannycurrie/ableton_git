1cd2a501b8ef885e2d3db8f69200332bf99e0958
dannycurrie <danny.currie42@gmail.com>
2019-06-05 22:49:31 +0100

    add basic error handling

--- ableton_git.sh
+++ ableton_git.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # functions required to use Git with Ableton projects
 
 # global vars
-FILENAME=$1
-MESSAGE=$2
 
 function checkFileValid() {
-  if [ -f "$FILENAME.als" ]; then
-    exit 0
-  else 
-    echo "$FILENAME is not a valid Ableton project file"
+  if [ ! -f "$1.als" ]; then
+    echo "$1 is not a valid Ableton project file"
+    exit 1
+  fi
+}
+
+function checkMessageValid() {
+  if [ -z $1 ]; then
+    echo "Commit message cannot be empty"
     exit 1
   fi
 }
 
 function commit() {
+  FILENAME=$1
+  MESSAGE=$2
+  checkFileValid $FILENAME
+  checkMessageValid $MESSAGE
   gzip -cd "$FILENAME.als" > "$FILENAME.xml"
   git add .
   git commit -m "$MESSAGE"
@@ -23,6 +30,8 @@ function commit() {
 }
 
 function checkout() {
+  FILENAME=$1
+  checkFileValid $FILENAME
   git checkout master
   gzip -c "$FILENAME.xml" > "$FILENAME.als"
   exit 0
