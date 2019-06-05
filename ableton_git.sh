4e4dac1c0c580928f8aad5597d4eabf8d94928bc
dannycurrie <danny.currie42@gmail.com>
2019-06-05 22:36:48 +0100

    ibniuniun

--- ableton_git.sh
+++ ableton_git.sh
@@ -3,10 +3,19 @@
 # functions required to use Git with Ableton projects
 
 # global vars
+FILENAME=$1
+MESSAGE=$2
+
+function checkFileValid() {
+  if [ -f "$FILENAME.als" ]; then
+    exit 0
+  else 
+    echo "$FILENAME is not a valid Ableton project file"
+    exit 1
+  fi
+}
 
 function commit() {
-  FILENAME=$1
-  MESSAGE=$2
   gzip -cd "$FILENAME.als" > "$FILENAME.xml"
   git add .
   git commit -m "$MESSAGE"
@@ -14,7 +23,6 @@ function commit() {
 }
 
 function checkout() {
-  FILENAME=$1
   git checkout master
   gzip -c "$FILENAME.xml" > "$FILENAME.als"
   exit 0
